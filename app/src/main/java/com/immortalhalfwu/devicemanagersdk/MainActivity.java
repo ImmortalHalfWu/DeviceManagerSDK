30b9e41a590e516a829935b437096ad3ee7359bd
v_wupeng <1426488898@qq.com>
2018-01-31 09:57:10 +0800

    [peng.wu] merge test3

--- app/src/main/java/com/immortalhalfwu/devicemanagersdk/MainActivity.java
+++ app/src/main/java/com/immortalhalfwu/devicemanagersdk/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String s = "666";
+        String s = "6";
     }
 }
